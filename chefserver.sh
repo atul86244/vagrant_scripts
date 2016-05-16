65450f92314a69fcc13533f15393fab5fb771782
Atul <atul@chef.io>
2016-05-16 13:19:24 +0530

    fix issues

--- chefserver.sh
+++ chefserver.sh
@@ -1,10 +1,14 @@
-`sudo yum install -y curl`
+`sudo yum install -y curl wget`
 
 `curl -L https://omnitruck.chef.io/install.sh | sudo bash -s -- -P server`
 
+`wget   https://packages.chef.io/stable/el/6/chef-server-core-12.6.0-1.el6.x86_64.rpm`
+
+`rpm -Uvh chef-server-core-12.6.0-1.el6.x86_64.rpm`
+
 `sudo chef-server-ctl reconfigure`
 
-`sudo chef-server-ctl user-create admin Atul Srivastava atul@chef.io 'admin' `
+`sudo chef-server-ctl user-create admin Atul Srivastava atul@chef.io 'admin123' `
 
 `sudo chef-server-ctl org-create demo 'Demo' --association_user admin`
 
