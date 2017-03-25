d54b6b44d1403e600a356ee89e3fd1544a8a29f0
Atul Srivastava <atulsrivastava86244@gmail.com>
2017-03-25 16:57:35 +0530

    Update chefserver.sh

--- chefserver.sh
+++ chefserver.sh
@@ -1,14 +1,14 @@
 `sudo yum install -y curl wget`
 
-`curl -L https://omnitruck.chef.io/install.sh | sudo bash -s -- -P server`
+#`curl -L https://omnitruck.chef.io/install.sh | sudo bash -s -- -P server`
 
-`wget   https://packages.chef.io/stable/el/6/chef-server-core-12.6.0-1.el6.x86_64.rpm`
+`wget   https://packages.chef.io/files/stable/chef-server/12.13.0/el/7/chef-server-core-12.13.0-1.el7.x86_64.rpm`
 
-`rpm -Uvh chef-server-core-12.6.0-1.el6.x86_64.rpm`
+`rpm -Uvh chef-server-core-12.13.0-1.el7.x86_64.rpm`
 
 `sudo chef-server-ctl reconfigure`
 
-`sudo chef-server-ctl user-create admin Atul Srivastava atul@chef.io 'admin123' `
+`sudo chef-server-ctl user-create admin Atul Srivastava atul@hpe.com 'admin123' `
 
 `sudo chef-server-ctl org-create demo 'Demo' --association_user admin`
 
